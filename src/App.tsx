4d500cebbcecad5d0fd7ce9e2f967ad5251a7a4a
zurukumo <zurukumo@gmail.com>
2021-11-14 04:30:50 +0900

    feat: 顔写真付きリスト表示機能beta

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import 'tailwind.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './Home'
+import List from './List'
 import Trade from './Trade'
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/trade" element={<Trade />} />
+      <Route path="/list" element={<List />} />
     </Routes>
   </BrowserRouter>
 );
