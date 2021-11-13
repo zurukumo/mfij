22ce7aae10fe9e7b1c54cdb5a00b41ef26fb36b1
matsuda.shinji <thepumpkin404@gmail.com>
2021-11-13 22:30:54 +0900

    fix: データを事前に定義する形式に変更

---
+++ src/Trade/datas.ts
@@ -0,0 +1,249 @@
+export const DATAS = [
+  {
+    date: "2021/07/29",
+    acquires: ["pick:2026-2", "cash:"],
+    releases: ["player:prestja01"],
+  },
+  {
+    date: "2021/03/25",
+    acquires: ["player:harriga01", "player:hamptrj01", "pick:2025-1"],
+    releases: ["player:gordoaa01", "player:clarkga01"],
+  },
+  {
+    date: "2021/03/25",
+    acquires: ["player:teaguje01", "pick:2025-2", "pick:2027-2"],
+    releases: ["player:fournev01"],
+  },
+  {
+    date: "2021/03/25",
+    acquires: [
+      "player:cartewe01",
+      "player:porteot01",
+      "pick:2021-1,player:wagnefr01",
+      "pick:2023-1",
+    ],
+    releases: ["player:vucevni01", "player:aminual01"],
+  },
+  {
+    date: "2020/11/18",
+    acquires: ["pick:2022-2", "pick:2026-2"],
+    releases: ["pick:2020-2,player:nworajo01"],
+  },
+  {
+    date: "2020/02/06",
+    acquires: ["player:ennisja01"],
+    releases: ["pick:2020-2,player:reedpa01"],
+  },
+  {
+    date: "2019/06/20",
+    acquires: ["pick:2020-2,player:reedpa01", "cash:"],
+    releases: ["pick:2019-2,player:hortota01"],
+  },
+  {
+    date: "2019/02/07",
+    acquires: ["player:fultzma01"],
+    releases: [
+      "player:simmojo02",
+      "pick:2019-2,player:edwarca01",
+      "pick:2020-1,player:maxeyty01",
+    ],
+  },
+  {
+    date: "2018/07/23",
+    acquires: ["player:martija01", "cash:"],
+    releases: ["player:johnsda04", "player:harvety01"],
+  },
+  {
+    date: "2018/07/20",
+    acquires: ["player:johnsda04", "cash:"],
+    releases: ["player:purviro01"],
+  },
+  {
+    date: "2018/07/07",
+    acquires: ["player:mozgoti01", "player:grantje02"],
+    releases: [
+      "player:biyombi01",
+      "pick:2019-2,player:martico01",
+      "pick:2020-2,player:madarya01",
+    ],
+  },
+  {
+    date: "2018/06/21",
+    acquires: ["player:jacksju02", "pick:2019-2,player:martico01"],
+    releases: ["player:vandeja01"],
+  },
+  {
+    date: "2018/02/08",
+    acquires: ["pick:2018-2,player:vandeja01"],
+    releases: ["player:paytoel01"],
+  },
+  {
+    date: "2017/06/22",
+    acquires: ["pick:2020-1,player:maxeyty01", "pick:2020-2,player:madarya01"],
+    releases: ["player:pasecan01"],
+  },
+  {
+    date: "2017/06/22",
+    acquires: ["pick:2019-2,player:hortota01"],
+    releases: ["player:rabbiv01"],
+  },
+  {
+    date: "2017/02/14",
+    acquires: ["player:rosste01", "pick:2017-1,player:pasecan01"],
+    releases: ["player:ibakase01"],
+  },
+  {
+    date: "2016/07/15",
+    acquires: ["player:wilcocj01", "cash:"],
+    releases: ["player:marblde01", "pick:2020-2,player:careyve01"],
+  },
+  {
+    date: "2016/07/07",
+    acquires: ["cash:"],
+    releases: ["player:napiesh01"],
+  },
+  {
+    date: "2016/06/29",
+    acquires: ["player:meeksjo01"],
+    releases: ["pick:2019-2,player:handsja01"],
+  },
+  {
+    date: "2016/06/23",
+    acquires: ["player:ibakase01"],
+    releases: ["player:oladivi01", "player:ilyaser01", "player:sabondo01"],
+  },
+  {
+    date: "2016/06/23",
+    acquires: ["pick:2019-2,player:handsja01", "cash:"],
+    releases: ["player:laymaja01"],
+  },
+  {
+    date: "2016/02/18",
+    acquires: ["player:cunnija01", "pick:2020-2,player:careyve01"],
+    releases: ["player:fryech01"],
+  },
+  {
+    date: "2016/02/16",
+    acquires: ["player:ilyaser01", "player:jennibr01"],
+    releases: ["player:harrito02"],
+  },
+  {
+    date: "2016/01/12",
+    acquires: ["player:harrijo01", "pick:2017-2,protected:"],
+    releases: ["pick:2020-2,protected:"],
+  },
+  {
+    date: "2015/07/27",
+    acquires: ["player:napiesh01", "cash:"],
+    releases: ["pick:2016-2,protected:"],
+  },
+  {
+    date: "2015/07/12",
+    acquires: ["pick:2020-2,protected:"],
+    releases: ["player:harklma01"],
+  },
+  {
+    date: "2015/07/09",
+    acquires: ["pick:2019-2,player:edwarca01", "cash:"],
+    releases: ["player:oquinky01", "pick:2019-2,player:brazdig01"],
+  },
+  {
+    date: "2015/06/24",
+    acquires: ["player:timmaja01"],
+    releases: ["player:ridnolu01"],
+  },
+  {
+    date: "2014/07/14",
+    acquires: [
+      "player:randoan01",
+      "pick:2015-2,player:harvety01",
+      "pick:2016-2,player:laymaja01",
+      "cash:",
+    ],
+    releases: ["player:rakovmi01"],
+  },
+  {
+    date: "2014/06/26",
+    acquires: ["player:paytoel01"],
+    releases: [
+      "player:saricda01",
+      "pick:2015-2,player:hernawi01",
+      "pick:2017-1,player:foxde01",
+    ],
+  },
+  {
+    date: "2014/06/26",
+    releases: ["player:fournev01", "player:marblde01"],
+    acquires: ["player:afflaar01"],
+  },
+  {
+    date: "2013/02/21",
+    acquires: [
+      "player:lambdo01",
+      "player:harrito02",
+      "player:udrihbe01",
+      "cash:",
+    ],
+    releases: ["player:redicjj01", "player:ayongu01", "player:smithis01"],
+  },
+  {
+    date: "2013/02/21",
+    acquires: ["player:warriha01"],
+    releases: ["player:mcrobjo01"],
+  },
+  {
+    date: "2012/08/10",
+    acquires: [
+      "player:mcrobjo01",
+      "player:eyengch01",
+      "player:harklma01",
+      "player:vucevni01",
+      "player:afflaar01",
+      "player:harrial01",
+      "pick:2013-2,player:osbyro01",
+      "pick:2014-1,player:saricda01",
+      "pick:2015-2,protected:",
+      "pick:2017-1,player:foxde01",
+      "pick:2017-2,player:iwundwe01",
+      "pick:2018-2,player:kurucro01",
+    ],
+    releases: [
+      "player:howardw01",
+      "player:duhonch01",
+      "player:clarkea01",
+      "player:richaja01",
+    ],
+  },
+  {
+    date: "2012/07/11",
+    acquires: ["player:ayongu01"],
+    releases: ["player:anderry01"],
+  },
+  {
+    date: "2011/12/12",
+    acquires: ["player:davisgl01", "player:wafervo01"],
+    releases: ["player:bassbr01"],
+  },
+  {
+    date: "2011/06/23",
+    acquires: ["player:harpeju01"],
+    releases: ["pick:2013-2,player:crabbal01", "pick:2014-2,player:harrijo01"],
+  },
+  {
+    date: "2010/12/18",
+    acquires: [
+      "player:arenagi01",
+      "player:richaja01",
+      "player:turkohe01",
+      "player:clarkea01",
+    ],
+    releases: [
+      "player:lewisra02",
+      "player:cartevi01",
+      "player:pietrmi01",
+      "player:gortama01",
+      "pick:2011-1,player:mirotni01",
+      "cash:",
+    ],
+  },
+];
