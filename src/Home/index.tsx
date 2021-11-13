387e59af0cb338131e280e6d07bfa9c8f51c9caf
matsuda.shinji <thepumpkin404@gmail.com>
2021-11-13 21:12:55 +0900

    refactor: 説明文変更

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -6,8 +6,12 @@ const Home = () => (
   <Template title="ホーム">
     <Link to="./trade" className="hover:opacity-75">
       <Card title="トレードの歴史">
-        <span>マジックの歴史をまとめてみました。</span>
-        <span></span>
+        <p>マジックのトレードの歴史をまとめてみました。</p>
+        <p>
+          あの時、相手に渡した指名権は結局誰になったんだろう？というような情報が楽しめます。
+        </p>
+        <p>それを知って落ち込むも良し、喜ぶも良しです。</p>
+        <p>多分落ち込むことのほうが多いです。</p>
       </Card>
     </Link>
   </Template>
