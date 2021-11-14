6a779f4eade91f106844dd37dcde18924afe7906
matsuda.shinji <thepumpkin404@gmail.com>
2021-11-14 20:56:26 +0900

    refactore: コンポーネント共通化

--- src/Trade/index.tsx
+++ src/Trade/index.tsx
@@ -1,46 +1,51 @@
 import Template from 'components/Template'
+import { HTMLAttributes } from 'react'
 
 import { DATAS } from './datas'
 import Detail from './Detail'
+
+const DateTd = (props: HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={`hidden md:flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/6 ${props.className}`}
+  >
+    {props.children}
+  </div>
+);
+
+const DetailTd = (props: HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={`flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/2 md:w-2/5 ${props.className}`}
+  >
+    {props.children}
+  </div>
+);
 
 const Trade = () => (
   <Template title="トレードの歴史">
     <div className="flex flex-col items-center w-4/5 font-bold">
       <div className="flex w-full justify-center">
-        <div className="hidden md:flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/6">
-          日付
-        </div>
-        <div className="flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/2 md:w-2/5">
-          獲得
-        </div>
-        <div className="flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/2 md:w-2/5">
-          放出
-        </div>
+        <DateTd>日付</DateTd>
+        <DetailTd>獲得</DetailTd>
+        <DetailTd>放出</DetailTd>
       </div>
 
       {DATAS.map((data, key) => (
         <div className="flex w-full justify-center" key={key}>
-          <div
-            className={`hidden md:flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/6 ${
-              data.type === "personal" && "bg-black text-white"
-            }`}
+          <DateTd
+            className={data.type === "personal" ? "bg-black text-white" : ""}
           >
             {data.date}
-          </div>
-          <div
-            className={`flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/2 md:w-2/5 ${
-              data.type === "personal" && "bg-black text-white"
-            }`}
+          </DateTd>
+          <DetailTd
+            className={data.type === "personal" ? "bg-black text-white" : ""}
           >
             <Detail datas={data.acquires} />
-          </div>
-          <div
-            className={`flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/2 md:w-2/5 ${
-              data.type === "personal" && "bg-black text-white"
-            }`}
+          </DetailTd>
+          <DetailTd
+            className={data.type === "personal" ? "bg-black text-white" : ""}
           >
             <Detail datas={data.releases} />
-          </div>
+          </DetailTd>
         </div>
       ))}
     </div>
