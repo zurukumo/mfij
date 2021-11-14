c3966cf931ceea1b6b3a25324217515ac50bc2e3
matsuda.shinji <thepumpkin404@gmail.com>
2021-11-14 17:34:38 +0900

    refactor: react-helmetからreact-helmet-asyncに移行

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>Magic Juice</title>
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
