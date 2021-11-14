c6bb009309542d22812731e44efc162d467be6e5
zurukumo <zurukumo@gmail.com>
2021-11-15 02:18:46 +0900

    refactor: Trade/indexをscss化

--- src/Trade/index.tsx
+++ src/Trade/index.tsx
@@ -1,51 +1,28 @@
+import './index.scss'
+
 import Template from 'components/Template'
-import { HTMLAttributes } from 'react'
 
 import { DATAS } from './datas'
 import Detail from './Detail'
 
-const DateTd = (props: HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={`hidden md:flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/6 ${props.className}`}
-  >
-    {props.children}
-  </div>
-);
-
-const DetailTd = (props: HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={`flex flex-col justify-around text-center py-3 border-2 border-solid divide-gray-100 w-1/2 md:w-2/5 ${props.className}`}
-  >
-    {props.children}
-  </div>
-);
-
 const Trade = () => (
   <Template title="トレードの歴史">
-    <div className="flex flex-col items-center w-4/5 font-bold">
-      <div className="flex w-full justify-center">
-        <DateTd>日付</DateTd>
-        <DetailTd>獲得</DetailTd>
-        <DetailTd>放出</DetailTd>
+    <div className="table">
+      <div className="tr">
+        <div className="date-td">日付</div>
+        <div className="detail-td">獲得</div>
+        <div className="detail-td">放出</div>
       </div>
 
       {DATAS.map((data, key) => (
-        <div className="flex w-full justify-center" key={key}>
-          <DateTd
-            className={data.type === "personal" ? "bg-black text-white" : ""}
-          >
-            {data.date}
-          </DateTd>
-          <DetailTd
-            className={data.type === "personal" ? "bg-black text-white" : ""}
-          >
+        <div className="tr" key={key}>
+          <div className={`date-td ${data.type}`}>{data.date}</div>
+          <div className={`detail-td ${data.type}`}>
             <Detail datas={data.acquires} />
-          </DetailTd>
-          <DetailTd
-            className={data.type === "personal" ? "bg-black text-white" : ""}
-          >
+          </div>
+          <div className={`detail-td ${data.type}`}>
             <Detail datas={data.releases} />
-          </DetailTd>
+          </div>
         </div>
       ))}
     </div>
