0601272ed06edb07762799a1ba371b3813d3f282
zurukumo <zurukumo@gmail.com>
2021-11-18 01:09:46 +0900

    refactor: Navbarのscss化

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,14 @@
 import logo from 'logo.png'
 import { Link } from 'react-router-dom'
 
+import styles from './Navbar.module.scss'
+
 const Navbar = () => (
-  <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-4">
-    <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <Link to="/" className="hover:opacity-75">
-        <img src={logo} alt="Orlando Magic Logo" className="h-8" />
-      </Link>
-      <Link
-        to="/"
-        className="font-semibold text-xl tracking-tight ml-5 hover:opacity-75"
-      >
-        Magic Juice
-      </Link>
-    </div>
+  <nav className={styles.wrapper}>
+    <Link to="/" className={styles.title}>
+      <img src={logo} alt="Orlando Magic Logo" className={styles.title_logo} />
+      <div className={styles.title_name}>Magic Juice</div>
+    </Link>
   </nav>
 );
 
