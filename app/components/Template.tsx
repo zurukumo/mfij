030c966265f260ef32d37504ed8d29185226e1bc
zurukumo <zurukumo@gmail.com>
2024-10-28 17:57:32 +0900

    feat: remixの導入

--- app/components/Template.tsx
+++ app/components/Template.tsx
@@ -1,16 +1,18 @@
+import { MetaFunction } from '@remix-run/node'
 import { ReactNode } from 'react'
-import { Helmet } from 'react-helmet-async'
 
 import { Header } from '~/components/Header'
 
 import { Footer } from './Footer'
 
+export const meta: MetaFunction = () => [
+  { title: 'Magic Fan in Japan' },
+  { name: 'description', content: '日本のマジックファンのためのサイトです。' },
+]
+
 export const Template = (props: { title: string; children: ReactNode }) => (
   <div className="flex min-h-dvh w-full bg-slate-50">
     <div className="flex w-full flex-col items-center">
-      <Helmet>
-        <title>{props.title} - Magic Fan in Japan</title>
-      </Helmet>
       <Header />
       <main className="mt-16 flex w-[375px] flex-1 flex-col items-center gap-y-8 py-8 lg:w-[1024px]">
         <h1 className="text-2xl lg:text-3xl">{props.title}</h1>
